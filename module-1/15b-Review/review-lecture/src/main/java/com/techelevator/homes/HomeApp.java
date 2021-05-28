7f1d68caf0dea184369408d6df19ab8f8c727e5c
Steve Carmichael <steve@techelevator.com>
2021-05-28 09:14:16 -0400

    added lecture starter code and assessment

---
+++ module-1/15b-Review/review-lecture/src/main/java/com/techelevator/homes/HomeApp.java
@@ -0,0 +1,14 @@
+package com.techelevator.homes;
+
+import java.util.List;
+
+public class HomeApp {
+
+
+	public static void main(String[] args) {
+         HomeApp app = new HomeApp();
+	}
+
+	
+
+}
